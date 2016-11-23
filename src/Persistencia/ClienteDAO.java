0a6ba7aad5377bb3181c3d951dc1995448741b67
pedrocasarao <pedrohmprado@gmail.com>
2016-11-22 22:05:35 -0200

    inserindo funcionalidade como createdb insert delete entre outras funcionalidade dentro da camada de persistencia

--- src/Persistencia/ClienteDAO.java
+++ src/Persistencia/ClienteDAO.java
@@ -13,7 +13,7 @@ import java.sql.*;
 
 public class ClienteDAO {
 
-    public static void createDB() {
+    public static void createConnection() {
         Connection c = null;
         try {
             Class.forName("org.sqlite.JDBC");
@@ -51,4 +51,112 @@ public class ClienteDAO {
     }
     System.out.println("Table created successfully");
     }
+    
+    public class SQLiteJDBC
+{
+  public  void createTable( )
+  {
+    Connection c = null;
+    Statement stmt = null;
+    try {
+      Class.forName("org.sqlite.JDBC");
+      c = DriverManager.getConnection("jdbc:sqlite:test.db");
+      System.out.println("Opened database successfully");
+
+      stmt = c.createStatement();
+      String sql = "CREATE TABLE COMPANY " +
+                   "(ID INT PRIMARY KEY     NOT NULL," +
+                   " NAME           TEXT    NOT NULL, " + 
+                   " AGE            INT     NOT NULL, " + 
+                   " ADDRESS        CHAR(50), " + 
+                   " SALARY         REAL)"; 
+      stmt.executeUpdate(sql);
+      stmt.close();
+      c.close();
+    } catch ( Exception e ) {
+      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+      System.exit(0);
+    }
+    System.out.println("Table created successfully");
+  }
+  
+ }
+
+
+
+  public void insert( String cliente)
+  {
+    Connection c = null;
+    Statement stmt = null;
+    try {
+      Class.forName("org.sqlite.JDBC");
+      c = DriverManager.getConnection("jdbc:sqlite:test.db");
+      c.setAutoCommit(false);
+      System.out.println("Opened database successfully");
+
+      stmt = c.createStatement();
+      String sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
+                   "VALUES (1, 'Paul', 32, 'California', 20000.00 );"; 
+      stmt.executeUpdate(sql);
+
+      sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
+            "VALUES (2, 'Allen', 25, 'Texas', 15000.00 );"; 
+      stmt.executeUpdate(sql);
+
+      sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
+            "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );"; 
+      stmt.executeUpdate(sql);
+
+      sql = "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) " +
+            "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );"; 
+      stmt.executeUpdate(sql);
+
+      stmt.close();
+      c.commit();
+      c.close();
+    } catch ( Exception e ) {
+      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+      System.exit(0);
+    }
+    System.out.println("Records created successfully");
+  }
+   public  void updateDB( String args[] )
+  {
+    Connection c = null;
+    Statement stmt = null;
+    try {
+      Class.forName("org.sqlite.JDBC");
+      c = DriverManager.getConnection("jdbc:sqlite:test.db");
+      c.setAutoCommit(false);
+      System.out.println("Opened database successfully");
+
+      stmt = c.createStatement();
+      String sql = "UPDATE COMPANY set SALARY = 25000.00 where ID=1;";
+      stmt.executeUpdate(sql);
+      c.commit();
+
+      ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
+      while ( rs.next() ) {
+         int id = rs.getInt("id");
+         String  name = rs.getString("name");
+         int age  = rs.getInt("age");
+         String  address = rs.getString("address");
+         float salary = rs.getFloat("salary");
+         System.out.println( "ID = " + id );
+         System.out.println( "NAME = " + name );
+         System.out.println( "AGE = " + age );
+         System.out.println( "ADDRESS = " + address );
+         System.out.println( "SALARY = " + salary );
+         System.out.println();
+      }
+      rs.close();
+      stmt.close();
+      c.close();
+    } catch ( Exception e ) {
+      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+      System.exit(0);
+    }
+    System.out.println("Operation done successfully");
+  }
+
 }
